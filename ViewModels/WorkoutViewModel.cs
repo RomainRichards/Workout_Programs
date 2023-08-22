27dd865765115e7d0667ecb8b64609ac8cfbca95
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-22 14:05:43 -0400

    Add save file functionality

--- ViewModels/WorkoutViewModel.cs
+++ ViewModels/WorkoutViewModel.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Xml.Linq;
 using Workout_Programs.Models;
+//using DocumentFormat.OpenXml.Packaging;
+//using DocumentFormat.OpenXml.Wordprocessing;
+//using DocumentFormat.OpenXml;
 
 namespace Workout_Programs.ViewModels
 {
@@ -20,8 +27,8 @@ namespace Workout_Programs.ViewModels
         }
         public event PropertyChangedEventHandler PropertyChanged;
 
-       
-        private WorkoutDetail _workoutDetail;
+       // Gets and Sets workout detail when adding a workout to the database. 
+        private WorkoutDetail _workoutDetail = new WorkoutDetail();
         public WorkoutDetail WorkoutDetail
         {
             get { return _workoutDetail; }
@@ -32,6 +39,7 @@ namespace Workout_Programs.ViewModels
             }
         }
 
+        // Request workout list from the database to then be loaded to view.
         private ObservableCollection<WorkoutDetail> lstWorkoutDetail;
         public ObservableCollection<WorkoutDetail> LstWorkoutDetail
         {
@@ -42,6 +50,8 @@ namespace Workout_Programs.ViewModels
                 OnPropertyChanged(nameof(LstWorkoutDetail));
             }
         }
+
+        // Gets and Sets selected workout details, to be edited.
         private WorkoutDetail _selectedWorkoutDetail;
 
         public WorkoutDetail SelectedWorkoutDetail
@@ -53,18 +63,6 @@ namespace Workout_Programs.ViewModels
                 OnPropertyChanged(nameof(SelectedWorkoutDetail));
             }
         }
-
-        private WorkoutDetail workoutDetail;
-
-        //public WorkoutDetail WorkoutDetail
-        //{
-        //    get { return workoutDetail; }
-        //    set
-        //    {
-        //        workoutDetail = value;
-        //        OnPropertyChanged(nameof(WorkoutDetail));
-        //    }
-        //}
 
 
         // Initialize database.
@@ -80,27 +78,40 @@ namespace Workout_Programs.ViewModels
             UpdateCommand = new Command((s) => true, Select_Update);
             AddWorkoutCommand = new Command((s) => true, AddWorkout);
             UpdateWorkoutCommand = new Command((s) => true, UpdateWorkout);
+           // SaveCommand = new Command((s) => true, SaveWorkout);
         }
+
+
 
         // Add workout to the database. 
         private void AddWorkout(object obj)
         {
+            WorkoutDetail.ID = "WO" + workoutProgramDBEntities.WorkoutDetails.Count();
             if(WorkoutDetail != null)
             {
                 workoutProgramDBEntities.WorkoutDetails.Add(WorkoutDetail);
                 workoutProgramDBEntities.SaveChanges();
                 WorkoutDetail = new WorkoutDetail();
+                LstWorkoutDetail.Add(WorkoutDetail);
             }
             else
             {
-                MessageBox.Show("Missing Entry");
+                MessageBox.Show("Missing workout information");
             }
         }
 
         // Save changes made after updating the workout to the View to the database. 
         private void UpdateWorkout(object obj)
         {
-            workoutProgramDBEntities.SaveChanges();
+            if(SelectedWorkoutDetail != null)
+            {
+                workoutProgramDBEntities.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("No workout selected");
+            }
+           
         }
 
         // Selects workout to update, populating the text box witht that workout details.
@@ -130,6 +141,49 @@ namespace Workout_Programs.ViewModels
         public ICommand UpdateCommand { get; set; }
         public ICommand UpdateWorkoutCommand { get; set; }
         public ICommand AddWorkoutCommand { get; set; }
+       // public ICommand SaveCommand { get; set; }
+
+
+
+
+        //private void SaveWorkout(object obj)
+        //{
+        //    // Check if a workout is selected
+        //    if (SelectedWorkoutDetail != null)
+        //    {
+        //        Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+        //        saveFileDialog.Filter = "Word Documents (*.docx)|*.docx|All Files (*.*)|*.*";
+
+        //        if (saveFileDialog.ShowDialog() == true)
+        //        {
+        //            string filePath = saveFileDialog.FileName;
+
+        //            // Create a Word document
+        //            using (WordprocessingDocument doc = WordprocessingDocument.Create(filePath, WordprocessingDocumentType.Document))
+        //            {
+        //                // Add a main document part
+        //                MainDocumentPart mainPart = doc.AddMainDocumentPart();
+        //                mainPart.Document = new Document();
+
+        //                // Create a body
+        //                Body body = mainPart.Document.AppendChild(new Body());
+
+        //                // Add workout details to the document
+        //                DocumentFormat.OpenXml.Drawing.Paragraph paragraph = body.AppendChild(new DocumentFormat.OpenXml.Drawing.Paragraph());
+        //                DocumentFormat.OpenXml.Drawing.Run run = paragraph.AppendChild(new DocumentFormat.OpenXml.Drawing.Run());
+        //                run.AppendChild(new Text($"Workout ID: {SelectedWorkoutDetail.ID}"));
+        //                // Add more details as needed
+
+        //                // Save the document
+        //                mainPart.Document.Save();
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("No workout selected");
+        //    }
+        //}
     }
 
     // Implement ICommand interface to call different commands to the view.
